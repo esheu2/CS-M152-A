0a896fdf70168d46f2469c2b3bf15ee1dbe85bba
Gajan Nagaraj <thegajannagaraj@gmail.com>
2019-03-07 15:46:03 -0800

    removed 2

--- lab2/lab2/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/152/CS-M152-A/lab2/lab2/tb_isim_beh.exe" -prj "C:/Users/152/CS-M152-A/lab2/lab2/tb_beh.prj" "work.tb" "work.glbl" 
